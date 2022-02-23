2682928d77fbadc8e4ef171dd4c8ee88ba3fd489
mahannajafi <mahan.nwork@gmail.com>
2022-02-23 12:56:19 +0330

    special item in left menu fixed

--- src/Components/LeftMenu.js
+++ src/Components/LeftMenu.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import '../Styles/LeftMenu.css'
-import { FaSpotify, FaEllipsisH } from "react-icons/fa";
+import { FaSpotify, FaEllipsisH, FaHeart } from "react-icons/fa";
 import { BiSearchAlt } from "react-icons/bi";
 import { Menu } from './Menu';
 import { MenuList } from './MenuList';
+
 
 
 function LeftMenu() {
@@ -19,29 +20,37 @@ function LeftMenu() {
       {/* <Menu title={''} MenuObject={MenuList} className={'leftMenuOption'} /> */}
       <div className='MenuContainer'>
         <ul>
-        <li>
+          <li>
             <a className='leftMenuOption' href="#">
-            <svg fill="currentColor" role="img" height="24" width="24" class="Svg-sc-1bi12j5-0 hDgDGI home-icon" viewBox="0 0 24 24"><path d="M12.5 3.247a1 1 0 00-1 0L4 7.577V20h4.5v-6a1 1 0 011-1h5a1 1 0 011 1v6H20V7.577l-7.5-4.33zm-2-1.732a3 3 0 013 0l7.5 4.33a2 2 0 011 1.732V21a1 1 0 01-1 1h-6.5a1 1 0 01-1-1v-6h-3v6a1 1 0 01-1 1H3a1 1 0 01-1-1V7.577a2 2 0 011-1.732l7.5-4.33z"></path></svg>              <span>Home</span>
+              <svg fill="currentColor" role="img" height="24" width="24" class="Svg-sc-1bi12j5-0 hDgDGI home-icon" viewBox="0 0 24 24"><path d="M12.5 3.247a1 1 0 00-1 0L4 7.577V20h4.5v-6a1 1 0 011-1h5a1 1 0 011 1v6H20V7.577l-7.5-4.33zm-2-1.732a3 3 0 013 0l7.5 4.33a2 2 0 011 1.732V21a1 1 0 01-1 1h-6.5a1 1 0 01-1-1v-6h-3v6a1 1 0 01-1 1H3a1 1 0 01-1-1V7.577a2 2 0 011-1.732l7.5-4.33z"></path></svg>
+              <span>Home</span>
             </a>
           </li>
           <li>
             <a className='leftMenuOption' href="#">
-            <svg fill="currentColor" role="img" height="24" width="24" class="Svg-sc-1bi12j5-0 hDgDGI search-icon" viewBox="0 0 24 24"><path d="M10.533 1.279c-5.18 0-9.407 4.14-9.407 9.279s4.226 9.279 9.407 9.279c2.234 0 4.29-.77 5.907-2.058l4.353 4.353a1 1 0 101.414-1.414l-4.344-4.344a9.157 9.157 0 002.077-5.816c0-5.14-4.226-9.28-9.407-9.28zm-7.407 9.279c0-4.006 3.302-7.28 7.407-7.28s7.407 3.274 7.407 7.28-3.302 7.279-7.407 7.279-7.407-3.273-7.407-7.28z"></path></svg>              <span>Search</span>
+              <svg fill="currentColor" role="img" height="24" width="24" class="Svg-sc-1bi12j5-0 hDgDGI search-icon" viewBox="0 0 24 24"><path d="M10.533 1.279c-5.18 0-9.407 4.14-9.407 9.279s4.226 9.279 9.407 9.279c2.234 0 4.29-.77 5.907-2.058l4.353 4.353a1 1 0 101.414-1.414l-4.344-4.344a9.157 9.157 0 002.077-5.816c0-5.14-4.226-9.28-9.407-9.28zm-7.407 9.279c0-4.006 3.302-7.28 7.407-7.28s7.407 3.274 7.407 7.28-3.302 7.279-7.407 7.279-7.407-3.273-7.407-7.28z"></path></svg>
+              <span>Search</span>
             </a>
           </li>
           <li>
             <a className='leftMenuOption' href="#">
-            <svg fill="currentColor" role="img" height="24" width="24" class="Svg-sc-1bi12j5-0 hDgDGI collection-icon" viewBox="0 0 24 24"><path d="M14.5 2.134a1 1 0 011 0l6 3.464a1 1 0 01.5.866V21a1 1 0 01-1 1h-6a1 1 0 01-1-1V3a1 1 0 01.5-.866zM16 4.732V20h4V7.041l-4-2.309zM3 22a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1zm6 0a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1z"></path></svg>              <span>Your Library</span>
+              <svg fill="currentColor" role="img" height="24" width="24" class="Svg-sc-1bi12j5-0 hDgDGI collection-icon" viewBox="0 0 24 24"><path d="M14.5 2.134a1 1 0 011 0l6 3.464a1 1 0 01.5.866V21a1 1 0 01-1 1h-6a1 1 0 01-1-1V3a1 1 0 01.5-.866zM16 4.732V20h4V7.041l-4-2.309zM3 22a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1zm6 0a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1z"></path></svg>
+              <span>Your Library</span>
             </a>
           </li>
           <li>
-            <a className='leftMenuOption leftMenuOption__bottom1' href="#">
-            <svg fill="currentColor"  role="img" height="24" width="24" class="Svg-sc-1bi12j5-0 hDgDGI collection-icon" viewBox="0 0 24 24"><path d="M14.5 2.134a1 1 0 011 0l6 3.464a1 1 0 01.5.866V21a1 1 0 01-1 1h-6a1 1 0 01-1-1V3a1 1 0 01.5-.866zM16 4.732V20h4V7.041l-4-2.309zM3 22a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1zm6 0a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1z"></path></svg>              <span className='special'>Create Playlist</span>
+            <a className='leftMenuOption leftMenuOption__bottom leftMenuOption__bottom1' href="#">
+              <div className='plus'><div>+</div></div>
+
+              <span className='special'>Create Playlist</span>
             </a>
           </li>
           <li>
             <a className='leftMenuOption leftMenuOption__bottom' href="#">
-            <svg className='liked' fill="currentColor"  role="img" height="12" width="12" aria-hidden="true" viewBox="0 0 16 16" class="Svg-sc-1bi12j5-0 hDgDGI"><path d="M15.724 4.22A4.313 4.313 0 0012.192.814a4.269 4.269 0 00-3.622 1.13.837.837 0 01-1.14 0 4.272 4.272 0 00-6.21 5.855l5.916 7.05a1.128 1.128 0 001.727 0l5.916-7.05a4.228 4.228 0 00.945-3.577z"></path></svg>              <span className='special'>Liked Songs</span>
+              <div className='favorite'>
+                <FaHeart size='12px' />
+              </div>
+              <span className='special'>Liked Songs</span>
             </a>
           </li>
         </ul>
